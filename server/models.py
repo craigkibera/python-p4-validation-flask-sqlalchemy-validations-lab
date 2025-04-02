7394ea76716503ef0ef23129f0ff423bca53516c
craigkibera <craigituro@gmail.com>
2025-04-02 09:23:02 +0300

    validations

--- server/models.py
+++ server/models.py
@@ -12,6 +12,45 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError("Author must have a name.")
+        existing_author = Author.query.filter_by(name=value).first()
+        if existing_author:
+            raise ValueError("Author name must be unique.")
+        return value
+
+    @validates('title')
+    def validate_title(self, key, value):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in value for phrase in clickbait_phrases):
+            raise ValueError("Post title must contain one of the following: 'Won't Believe', 'Secret', 'Top', 'Guess'.")
+        return value
+
+    @validates('category')
+    def validate_category(self, key, value):
+        if value not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Post category must be either 'Fiction' or 'Non-Fiction'.")
+        return value
+
+    @validates('summary')
+    def validate_summary(self, key, value):
+        if len(value) > 250:
+            raise ValueError("Post summary must be a maximum of 250 characters.")
+        return value
+
+    @validates('content')
+    def validate_content(self, key, value):
+        if len(value) < 250:
+            raise ValueError("Post content must be at least 250 characters long.")
+        return value
+
+    @validates('phone_number')
+    def validate_phone_number(self, key, value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Phone number must be exactly ten digits.")
+        return value
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,6 +67,32 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('title')
+    def validate_title(self, key, value):
+        if not value:
+            raise ValueError("Post must have a title.")
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in value for phrase in clickbait_phrases):
+            raise ValueError("Post title must contain one of the following: 'Won't Believe', 'Secret', 'Top', 'Guess'.")
+        return value
+
+    @validates('content')
+    def validate_content(self, key, value):
+        if not value or len(value) < 250:
+            raise ValueError("Post content must be at least 250 characters long.")
+        return value
+
+    @validates('summary')
+    def validate_summary(self, key, value):
+        if not value or len(value) > 250:
+            raise ValueError("Post summary must be a maximum of 250 characters.")
+        return value
+
+    @validates('category')
+    def validate_category(self, key, value):
+        if value not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Post category must be either 'Fiction' or 'Non-Fiction'.")
+        return value
 
 
     def __repr__(self):
